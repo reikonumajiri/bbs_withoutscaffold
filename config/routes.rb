35005c06fa8cebb8676fbfaab462f9d0cdfdce75
reikonumajiri <reiko.numajiri@finc.com>
2019-09-19 15:19:16 +0900

    test up

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+# Rails.application.routes.draw do
+  devise_for :users
+#   resources :messages
+#   resources :boards
+#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# end
+
+Rails.application.routes.draw do
+  devise_for :users
+  resources :boards do
+    resources :messages
+  end
+end
